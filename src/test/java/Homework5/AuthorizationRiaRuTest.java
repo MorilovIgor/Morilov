eda619460dbb918ca71c8e2b35dad7863d0ad1b3
Igor <miv@d-element.ru>
2022-03-01 12:59:06 +0500

    HW5 start

--- src/test/java/Homework5/AuthorizationRiaRuTest.java
+++ src/test/java/Homework5/AuthorizationRiaRuTest.java
@@ -5,15 +5,23 @@ import org.example.DriverManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class AuthorizationRiaRuTest extends BaseTest {
 
     WebDriver driver = DriverManager.getDriver();
+    Actions actions = new Actions(driver);
 
     @Test
     @DisplayName("Авторизация")
     public void login() {
+        driver.get("https://ria.ru");
 
+        // как обратиться к элементам???
+        actions
+                .click()
+                .build()
+                .perform();
     }
 
     @Test
